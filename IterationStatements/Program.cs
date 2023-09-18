a11f6cb0ddd5d940118471ddd2fc43173296e420
CLochstampfor60 <cflochjr@gmail.com>
2023-09-18 17:12:15 -0400

    exercise complete

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -5,6 +5,7 @@ namespace IterationStatements
 {
     public class Program
     {
+
         //LukeWarm Section: Create methods below
         //Write a method that will print to the console all numbers 1000 through - 1000
         public static void PrintThousand()
@@ -12,7 +13,8 @@ namespace IterationStatements
             for (int i = 1000; i >= -1000; i--)
             {
                 Console.WriteLine(i);
-            }
+             }
+            Console.WriteLine("\n");
         }
 
         //Write a method that will print to the console numbers 3 through 999 by 3 each time
@@ -25,9 +27,9 @@ namespace IterationStatements
         }
 
         //Write a method to accept two integers as parameters and check whether they are equal or not
-        public static void PrintCompareIntegers()
+        public static void CompareIntegers()
         {
-            Console.WriteLine("Please choose a number:");
+            Console.WriteLine("\n Please choose a number:");
             var a = Console.ReadLine();
             Console.WriteLine("Please choose another number:");
             var b = Console.ReadLine();
@@ -35,28 +37,26 @@ namespace IterationStatements
             var variablesEqual = (a == b) ? "The variables are equal." : "The variables are NOT equal.";
             Console.WriteLine(variablesEqual);
         }
-    }
 
-    //Write a method to check whether a given number is even or odd
-    public static void CheckEvenOrOdd()
+
+        //Write a method to check whether a given number is even or odd
+        public static void EvenOrOdd()
         {
-            Console.WriteLine("Please choose a number:");
-            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n Please choose a number:");
+            int g = int.Parse(Console.ReadLine());
 
-            var evenOdd = (a % 2 == 0) ? "The number is even.") : "The number is odd.";
+            var evenOdd = (g % 2 == 0) ? "The number is even." : "The number is odd.";
             Console.WriteLine(evenOdd);
         }
 
-    }
 
-
-//Write a method to check whether a given number is positive or negative
-public static void CheckPositiveOrNegative()
+        //Write a method to check whether a given number is positive or negative
+        public static void CheckPositiveOrNegative()
         {
-            Console.WriteLine("Please choose a number between -100 and 100:");
-            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n Please choose a number between -100 and 100:");
+            int r = int.Parse(Console.ReadLine());
 
-            var positiveNumber = (a >= 0) ? "The number is positive." : "The number is negative.";
+            var positiveNumber = (r >= 0) ? "The number is positive." : "The number is negative.";
 
             Console.WriteLine(positiveNumber);
 
@@ -66,10 +66,9 @@ public static void CheckPositiveOrNegative()
         //Hint: Use Parse or the safer TryParse() for an extra challenge
         //Parse()
         //TryParse()
-
         public static void AgeToVote()
         {
-            Console.WriteLine("What is your age?");
+            Console.WriteLine("\n What is your age?");
             int a = int.Parse(Console.ReadLine());
 
             var legalAge = (a >= 18) ? "Congrats! You ARE legally old enough to vote in the U.S." : "Sorry, you are NOT legally old enough to vote in the U.S.";
@@ -82,13 +81,12 @@ public static void CheckPositiveOrNegative()
         //Write a method to check if an integer(from the user) is in the range -10 to 10
         public static void PrintNumberBetween()
         {
-            Console.WriteLine("Choose a number between -10 and 10:");
+            Console.WriteLine("\n Choose a number between -10 and 10:");
             int a = int.Parse(Console.ReadLine());
 
             if (-10 <= a && a <= 10)
             {
                 Console.WriteLine($"{a} is BETWEEN -10 and 10.");
-
             }
             else if (-10 >= a)
             {
@@ -98,14 +96,13 @@ public static void CheckPositiveOrNegative()
             {
                 Console.WriteLine($"Sorry, {a} is GREATER than 10.");
             }
-
-
         }
+
 
         //Write a method to display the multiplication table(from 1 to 12) of a given integer
         public static void MultiplyYourNumber()
         {
-            Console.WriteLine("Choose any number:");
+            Console.WriteLine("\n Choose any number:");
             int a = int.Parse(Console.ReadLine());
 
             Console.Write($"Multiplication Table: {a} * (1 to 12)...\n");
@@ -114,27 +111,21 @@ public static void CheckPositiveOrNegative()
             {
                 var newNumber = a * e;
                 Console.Write($"{newNumber}\n");
-
             }
-
         }
-
 
         //Call the methods to test them in the Main method below
         static void Main(string[] args)
         {
-
             PrintThousand();
             PrintEveryThird();
-            PrintCompareIntegers();
-            CheckEvenOrOdd();
+            CompareIntegers();
+            EvenOrOdd();
             CheckPositiveOrNegative();
             AgeToVote();
             PrintNumberBetween();
             MultiplyYourNumber();
-
         }
-
 
     }
 }
